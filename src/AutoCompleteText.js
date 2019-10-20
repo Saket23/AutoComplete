d53b1a126fe9a5507e2b2abe6606e3fa74583d4e
saket-synup <saket.s@synup.com>
2019-10-20 19:13:43 +0530

    small fix

--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -45,6 +45,7 @@ export default class AutoCompleteText extends PureComponent {
   render() {
     return (
       <div className="AutoComplete">
+        <div>saket</div>
         <input
           value={this.state.text}
           type="text"
